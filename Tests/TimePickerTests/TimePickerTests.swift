cb94d1c3e375c1f961fd4050d130dac438ff030e
Bosco Ho <boscojwho@gmail.com>
2024-10-31 14:42:05 -0700

    + Basic unit tests.

--- Tests/TimePickerTests/TimePickerTests.swift
+++ Tests/TimePickerTests/TimePickerTests.swift
@@ -2,11 +2,30 @@ import XCTest
 @testable import TimePicker
 
 final class TimePickerTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func testNextPreviousCycle() {
+        XCTAssertEqual(TimePickerComponents.hour.next(), TimePickerComponents.minute)
+        XCTAssertEqual(TimePickerComponents.minute.next(), TimePickerComponents.second)
+        XCTAssertEqual(TimePickerComponents.second.next(), TimePickerComponents.hour)
+        
+        XCTAssertEqual(TimePickerComponents.hour.previous(), TimePickerComponents.second)
+        XCTAssertEqual(TimePickerComponents.minute.previous(), TimePickerComponents.hour)
+        XCTAssertEqual(TimePickerComponents.second.previous(), TimePickerComponents.minute)
+    }
+    
+    func testDateComponents() {
+        let hourComponents = TimePickerComponents.hour.dateComponents()
+        XCTAssertEqual(hourComponents.hour, 0)
+        XCTAssertNil(hourComponents.minute)
+        XCTAssertNil(hourComponents.second)
+        
+        let minuteComponents = TimePickerComponents.minute.dateComponents()
+        XCTAssertEqual(minuteComponents.minute, 0)
+        XCTAssertNil(minuteComponents.hour)
+        XCTAssertNil(minuteComponents.second)
+        
+        let secondComponents = TimePickerComponents.second.dateComponents()
+        XCTAssertEqual(secondComponents.second, 0)
+        XCTAssertNil(secondComponents.hour)
+        XCTAssertNil(secondComponents.minute)
     }
 }
